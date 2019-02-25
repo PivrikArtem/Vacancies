2b5c25f633c12776d68b7d668fd4e9ced83fa764
Artem Pivrik <artempivrik90@gmail.com>
2019-02-25 18:29:30 +0200

    add new css and ajax

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ function hideIcon() {
         i.style.display = 'block';
     }
 }
+
+
+const headers = {
+    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'accept': 'application/json'
+};
+
+const corsMode = 'no-cors';
+function requestData(url, type, body) {
+    return fetch(url, {
+        method: type,
+        body: body,
+        mode: corsMode
+    }).then(result => result.json());
+}
+
+const a=requestData('https://www.google.com/','GET');
+
+
+// $.ajax(
+//     'https://repetitora.net/',
+//     (data)=>{console.log(data)}
+//     );
